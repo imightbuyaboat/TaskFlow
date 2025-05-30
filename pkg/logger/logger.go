098f4d7808e23b39235aaed4aa1d557a91194739
imightbuyaboat <vovkbp@gmail.com>
2025-05-31 00:12:23 +0300

    формат времени в логах изменен на ISO8601

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,8 +2,10 @@ package logger
 
 import (
 	"sync"
+	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
@@ -13,8 +15,14 @@ var (
 
 func InitLogger() {
 	once.Do(func() {
+		config := zap.NewProductionConfig()
+		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		config.EncoderConfig.EncodeTime = func(t time.Time, pae zapcore.PrimitiveArrayEncoder) {
+			pae.AppendString(t.UTC().Format("2006-01-02T15:04:05.000Z"))
+		}
+
 		var err error
-		logger, err = zap.NewProduction()
+		logger, err = config.Build()
 		if err != nil {
 			panic("cannot initialize zap logger: " + err.Error())
 		}
